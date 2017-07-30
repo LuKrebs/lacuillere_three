f5125cd56e7b0a50d06273a404394f4d08a5df3e
Luciano Krebs <luciano.krebs@hotmail.com>
2017-07-30 09:01:19 -0300

    add restaurant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+
+  get 'contact', to: 'pages#contact'
+
+  get 'about', to: 'pages#about'
+
+  get 'hello', to: 'pages#welcome', as: :welcome
+
+  root to: 'pages#home'
+
+  get 'index', to: 'restaurants#index'
+
+  post 'create', to: 'restaurants#create'
+
+  get 'restaurants/:id', to: 'restaurants#show'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
